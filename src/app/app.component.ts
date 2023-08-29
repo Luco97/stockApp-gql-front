1d70b1ca88a5067672a1aa7add245e0f77a0569d
Luco97 <renatoluco@gmail.com>
2023-08-28 22:58:38 -0400

    ADD logica para manipulacion de tema oscuro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stockApp';
+
+  @HostBinding('class') theme = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.theme = window.localStorage.getItem('theme') || '';
+  }
+
+  themeChange() {
+    if (this.theme.includes('my-light-theme'))
+      this.theme = this.theme.replace('my-light-theme', '').trim();
+    else {
+      this.theme = this.theme.concat(' my-light-theme');
+      window.localStorage.setItem('theme', 'my-light-theme');
+    }
+  }
 }
